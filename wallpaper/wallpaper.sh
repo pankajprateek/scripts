a98598766c73e11bcc6a4d39f301ba0b26907fa8
Pankaj Prateek <pankaj200292@gmail.com>
2013-12-28 13:17:52 +0530

    python to python2

--- wallpaper/wallpaper.sh
+++ wallpaper/wallpaper.sh
@@ -1,11 +1,11 @@
 ip link show up >  /tmp/networklink
-x=`python check_network.py`
+x=`python2 check_network.py`
 if [ "$x" -ge 0 ] ; then
     # rm ~/.wallpaper/bing
     # rm ~/.wallpaper/wallpaper
     wget -q --proxy-user=deepu --proxy-password=srujana http://bing.com -O ~/scripts/wallpaper/bing
     # wget http://www.bing.com -O ~/.wallpaper/bing
-    x=`python ~/scripts/wallpaper//wallpaper.py`
+    x=`python2 ~/scripts/wallpaper//wallpaper.py`
     wget -q $x --proxy-user=deepu --proxy-password=srujana -O ~/scripts/wallpaper/wallpaper.jpg
 fi
 feh --bg-scale /home/pankaj/scripts/wallpaper/wallpaper.jpg & 
